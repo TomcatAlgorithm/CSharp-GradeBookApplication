fc0d600d236d3e195ab1d6d28f5502ad7e3c7c92
TomcatAlgorithm <banica.g@gmail.com>
2018-07-18 08:35:48 +0300

    Added 2 new classes to repressent 2 new Grade Book Types.

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,13 @@
+﻿
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook:StandardGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
